dc8cc9e568dd00be8294b9c2ca311f628a44e04a
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-02-03 21:01:32 +0500

    ShouldAddGuestAsync -> FAIL

--- Sheenam.Api.Infrastructure.Build/Program.cs
+++ Sheenam.Api.Infrastructure.Build/Program.cs
@@ -5,9 +5,9 @@
 
 using System.Collections.Generic;
 using ADotNet.Clients;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
 
 public class Program
 {
