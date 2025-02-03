6eecb2bb702f9369d8e8da1c4051ec28b36b8bff
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-02-03 22:42:34 +0500

    ShouldAddGuestAsync -> PASS

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -5,6 +5,7 @@
 
 using System.Threading.Tasks;
 using FluentAssertions;
+using Force.DeepCloner;
 using Moq;
 using Sheenam.Api.Models.Foundations.Guests;
 
@@ -19,7 +20,7 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
             Guest randomGuest = CreateRandomGuest();
             Guest inputGuest = randomGuest;
             Guest returningGuest = inputGuest;
-            Guest expectedGuest = returningGuest;
+            Guest expectedGuest = returningGuest.DeepClone();
 
             this.storageBrokerMock.Setup(broker =>
                 broker.InsertGuestAsync(inputGuest))
