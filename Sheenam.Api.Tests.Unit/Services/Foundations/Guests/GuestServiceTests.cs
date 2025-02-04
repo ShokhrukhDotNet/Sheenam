4cab008415bcf0660d026efb69dcdecd918b5948
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-02-04 14:56:12 +0500

    CODE RUB: Add Logging Broker

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -5,6 +5,7 @@
 
 using System;
 using Moq;
+using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Services.Foundations.Guests;
@@ -15,14 +16,17 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
     public partial class GuestServiceTests
     {
         private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly IGuestService guestService;
 
         public GuestServiceTests()
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
-            this.guestService =
-                new GuestService(storageBroker: this.storageBrokerMock.Object);
+            this.guestService = new GuestService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
         }
 
         private static Guest CreateRandomGuest() =>
