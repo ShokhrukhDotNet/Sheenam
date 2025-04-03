24a48764f6e95bcba1c8f08e1fac4829b2064c3e
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-03 21:19:36 +0500

    ShouldThrowNotFoundExceptionOnRemoveHostByIdIsNotFoundAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validation.RemoveById.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validation.RemoveById.cs
@@ -56,5 +56,49 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveHostByIdIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid inputHostId = Guid.NewGuid();
+            Host noHost = null;
+
+            var notFoundHostException =
+                new NotFoundHostException(inputHostId);
+
+            var expectedHostValidationException =
+                new HostValidationException(notFoundHostException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHostByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noHost);
+
+            // when
+            ValueTask<Host> removeHostById =
+                this.hostService.RemoveHostByIdAsync(inputHostId);
+
+            var actualHostValidationException =
+                await Assert.ThrowsAsync<HostValidationException>(
+                    removeHostById.AsTask);
+
+            // then
+            actualHostValidationException.Should()
+                .BeEquivalentTo(expectedHostValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHostByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHostValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteHostAsync(It.IsAny<Host>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
