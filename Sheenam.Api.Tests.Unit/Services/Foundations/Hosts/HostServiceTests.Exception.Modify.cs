8b1ea99138f817fea14744305d1ad3ad3856d232
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-03 16:54:57 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Exception.Modify.cs
@@ -0,0 +1,63 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
+using Sheenam.Api.Models.Foundations.Hosts;
+using System.Threading.Tasks;
+using System;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Host randomHost = CreateRandomHost();
+            Host someHost = randomHost;
+            Guid hostId = someHost.Id;
+            SqlException sqlException = GetSqlError();
+
+            var failedHostStorageException =
+                new FailedHostStorageException(sqlException);
+
+            var expectedHostDependencyException =
+                new HostDependencyException(failedHostStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHostByIdAsync(hostId)).Throws(sqlException);
+
+            // when
+            ValueTask<Host> modifyHostTask =
+                this.hostService.ModifyHostAsync(someHost);
+
+            HostDependencyException actualHostDependencyException =
+                await Assert.ThrowsAsync<HostDependencyException>(
+                    modifyHostTask.AsTask);
+
+            // then
+            actualHostDependencyException.Should()
+                .BeEquivalentTo(expectedHostDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedHostDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHostByIdAsync(hostId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateHostAsync(someHost), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
