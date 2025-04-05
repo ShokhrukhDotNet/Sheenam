9f476834f82335d1744cc94d7b1a5fc56c061623
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-05 18:28:14 +0500

    ShouldAddHomeAsync -> FAIL

---
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -0,0 +1,34 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using System;
+using System.Threading.Tasks;
+using Sheenam.Api.Brokers.DateTimes;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Homes;
+
+namespace Sheenam.Api.Services.Foundations.Homes
+{
+    public class HomeService : IHomeService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public HomeService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Home> AddHomeAsync(Home home) =>
+            throw new NotImplementedException();
+    }
+}
