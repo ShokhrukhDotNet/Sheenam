9c7ea440e31e2f1970b4d24e7849e4bdc9ea27fb
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-08 14:48:55 +0500

    BROKERS: Select Home

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Homes.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Homes.cs
@@ -3,6 +3,8 @@
 // Free To Use To Find Comfort and Pease
 //==================================================
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Sheenam.Api.Models.Foundations.Homes;
 
@@ -11,5 +13,7 @@ namespace Sheenam.Api.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Home> InsertHomeAsync(Home home);
+        IQueryable<Home> SelectAllHomes();
+        ValueTask<Home> SelectHomeByIdAsync(Guid homeId);
     }
 }
--- Sheenam.Api/Brokers/Storages/StorageBroker.Homes.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Homes.cs
@@ -3,6 +3,8 @@
 // Free To Use To Find Comfort and Pease
 //==================================================
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Sheenam.Api.Models.Foundations.Homes;
@@ -15,5 +17,10 @@ namespace Sheenam.Api.Brokers.Storages
 
         public async ValueTask<Home> InsertHomeAsync(Home home) =>
             await InsertAsync(home);
+
+        public IQueryable<Home> SelectAllHomes() => SelectAll<Home>();
+
+        public async ValueTask<Home> SelectHomeByIdAsync(Guid homeId) =>
+            await SelectAsync<Home>(homeId);
     }
 }
