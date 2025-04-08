7f2407350f09ec1e64bba17d074b501f5ace4e4a
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-08 15:58:50 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Exception.RetrieveAll.cs
@@ -0,0 +1,54 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Homes.Exceptions;
+using System;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedHomeStorageException =
+                new FailedHomeStorageException(sqlException);
+
+            var expectedHomeDependencyException =
+                new HomeDependencyException(failedHomeStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllHomes()).Throws(sqlException);
+
+            // when
+            Action retrieveAllHomesAction = () =>
+                this.homeService.RetrieveAllHomes();
+
+            HomeDependencyException actualHomeDependencyException =
+                Assert.Throws<HomeDependencyException>(retrieveAllHomesAction);
+
+            // then
+            actualHomeDependencyException.Should()
+                .BeEquivalentTo(expectedHomeDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllHomes(), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedHomeDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
