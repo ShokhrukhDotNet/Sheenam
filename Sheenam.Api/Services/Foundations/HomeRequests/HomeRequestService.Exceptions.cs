7d844182e9ea52314ee78b494126aba585a62d1b
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-13 17:33:16 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.Exceptions.cs
+++ Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.Exceptions.cs
@@ -7,6 +7,7 @@ using Sheenam.Api.Models.Foundations.HomeRequests.Exceptions;
 using Sheenam.Api.Models.Foundations.HomeRequests;
 using System.Threading.Tasks;
 using Xeptions;
+using Microsoft.Data.SqlClient;
 
 namespace Sheenam.Api.Services.Foundations.HomeRequests
 {
@@ -28,6 +29,12 @@ namespace Sheenam.Api.Services.Foundations.HomeRequests
             {
                 throw CreateAndLogValidationException(invalidHomeRequestException);
             }
+            catch (SqlException sqlException)
+            {
+                var failedHomeRequestStorageException = new FailedHomeRequestStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedHomeRequestStorageException);
+            }
         }
 
         private HomeRequestValidationException CreateAndLogValidationException(Xeption exception)
@@ -38,6 +45,14 @@ namespace Sheenam.Api.Services.Foundations.HomeRequests
             this.loggingBroker.LogError(homeRequestValidationException);
 
             return homeRequestValidationException;
+        }
+
+        private HomeRequestDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var homeRequestDependencyException = new HomeRequestDependencyException(exception);
+            this.loggingBroker.LogCritical(homeRequestDependencyException);
+
+            return homeRequestDependencyException;
         }
     }
 }
