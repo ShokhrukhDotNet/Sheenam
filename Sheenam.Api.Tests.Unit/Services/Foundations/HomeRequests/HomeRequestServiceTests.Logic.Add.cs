f8139ff9ede4c8dbf3bb99ed78e711212a7981f4
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-13 12:10:35 +0500

    ShouldAddHomeRequestAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Logic.Add.cs
@@ -0,0 +1,43 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddHomeRequestAsync()
+        {
+            // given
+            HomeRequest randomHomeRequest = CreateRandomHomeRequest();
+            HomeRequest inputHomeRequest = randomHomeRequest;
+            HomeRequest storageHomeRequest = inputHomeRequest;
+            HomeRequest expectedHomeRequest = storageHomeRequest;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertHomeRequestAsync(inputHomeRequest))
+                    .ReturnsAsync(storageHomeRequest);
+
+            // when
+            HomeRequest actualHomeRequest =
+                await this.homeRequestService.AddHomeRequestAsync(inputHomeRequest);
+
+            // then
+            actualHomeRequest.Should().BeEquivalentTo(expectedHomeRequest);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHomeRequestAsync(inputHomeRequest),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.cs
@@ -0,0 +1,52 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using System;
+using Moq;
+using Sheenam.Api.Brokers.DateTimes;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+using Sheenam.Api.Services.Foundations.HomeRequests;
+using Tynamix.ObjectFiller;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IHomeRequestService homeRequestService;
+
+        public HomeRequestServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.homeRequestService = new HomeRequestService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static HomeRequest CreateRandomHomeRequest() =>
+            CreateHomeRequestFiller(date: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<HomeRequest> CreateHomeRequestFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<HomeRequest>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+    }
+}
