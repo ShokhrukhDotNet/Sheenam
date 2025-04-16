7ad34749cc58ef12c11f27e89d14f5b27e90d4b5
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-16 20:04:09 +0500

    ShouldThrowValidationExceptionOnModifyIfHomeRequestIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Validation.Modify.cs
@@ -0,0 +1,50 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using Moq;
+using Sheenam.Api.Models.Foundations.HomeRequests.Exceptions;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfHomeRequestIsNullAndLogItAsync()
+        {
+            // given
+            HomeRequest nullHomeRequest = null;
+            var nullHomeRequestException = new NullHomeRequestException();
+
+            var expectedHomeRequestValidationException =
+                new HomeRequestValidationException(nullHomeRequestException);
+
+            // when
+            ValueTask<HomeRequest> modifyHomeRequestTask =
+                this.homeRequestService.ModifyHomeRequestAsync(nullHomeRequest);
+
+            HomeRequestValidationException actualHomeRequestValidationException =
+                await Assert.ThrowsAsync<HomeRequestValidationException>(
+                    modifyHomeRequestTask.AsTask);
+
+            // then
+            actualHomeRequestValidationException.Should()
+                .BeEquivalentTo(expectedHomeRequestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeRequestValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateHomeRequestAsync(It.IsAny<HomeRequest>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
