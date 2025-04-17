54a04a4afe011256ba637d4632b07875555e99c7
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-04-17 18:08:26 +0500

    ShouldRemoveHomeRequestById -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Logic.RemoveById.cs
@@ -0,0 +1,55 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//==================================================
+
+using Moq;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+using System.Threading.Tasks;
+using System;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveHomeRequestById()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guid inputHomeRequestId = randomId;
+            HomeRequest randomHomeRequest = CreateRandomHomeRequest();
+            HomeRequest storageHomeRequest = randomHomeRequest;
+            HomeRequest expectedInputHomeRequest = storageHomeRequest;
+            HomeRequest deletedHomeRequest = expectedInputHomeRequest;
+            HomeRequest expectedHomeRequest = deletedHomeRequest;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeRequestByIdAsync(inputHomeRequestId))
+                    .ReturnsAsync(storageHomeRequest);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteHomeRequestAsync(expectedInputHomeRequest))
+                    .ReturnsAsync(deletedHomeRequest);
+
+            // when
+            HomeRequest actualHomeRequest = await this
+                .homeRequestService.RemoveHomeRequestByIdAsync(randomId);
+
+            // then
+            actualHomeRequest.Should().BeEquivalentTo(expectedHomeRequest);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeRequestByIdAsync(inputHomeRequestId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteHomeRequestAsync(expectedInputHomeRequest),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
